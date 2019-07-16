94b3aeda76ef5926ab20011ed7d804b7c766a8c6
Lindvall Katri <s1900145@edu.bc.fi>
2019-07-16 17:05:00 +0300

    Updates

---
+++ precache-manifest.2b982510e07dd6b7722c6177c64fb18d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f38d581e392c021a84d46572a98cbaf",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "575dccf0629d12cf3b99",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2435d09af34b0911f63f",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "575dccf0629d12cf3b99",
+    "url": "/contactmanager/static/js/2.c48b6df6.chunk.js"
+  },
+  {
+    "revision": "2435d09af34b0911f63f",
+    "url": "/contactmanager/static/js/main.e30decfb.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
